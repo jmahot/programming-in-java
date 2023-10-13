2021adc3b588c029a67896c557d51b923281589e
Julie MAHOT <julie.mahot@outlook.fr>
2023-10-13 03:46:00 +0200

    Commit

--- lab03/src/main/java/agh/ii/prinjava/lab03/exc03_01/QueueOfInts.java
+++ lab03/src/main/java/agh/ii/prinjava/lab03/exc03_01/QueueOfInts.java
@@ -3,18 +3,20 @@ package agh.ii.prinjava.lab03.exc03_01;
 /** QueueOfInts is an interface for a queue of integers.
  */
 public interface QueueOfInts {
-    /** Add an integer to the end of the queue. */
+    /** @param x Add an integer to the end of the queue. */
     void enqueue(int x);
 
-    /** Remove and return the integer from the front of the queue. */
+    /** @return the integer removed from the front of the queue. */
     int dequeue();
 
-    /** Check if the queue is empty. */
+    /** Check if the queue is empty.
+     * @return true if the queue is empty and if not, it returns false.*/
     boolean isEmpty();
 
-    /** Get the number of elements in the queue. */
+    /** @return The number of elements in the queue. */
     int numOfElems();
 
-    /** Peek at the integer at the front of the queue without removing it. */
+    /** Peek at the integer at the front of the queue without removing it.
+     * @return this integer. */
     int peek();
 }
